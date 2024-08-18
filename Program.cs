e6a11daa0f79953c4e0bea46e96534b6fd257650
Avedev444 <gitavedev@gmail.com>
2024-08-18 18:16:29 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Registro_Funcionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
